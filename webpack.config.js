54deed0c1dba8111596ea02773127df175efa608
Anastasiia Gorbenko <nstgorbenko@yandex.ru>
2020-06-09 14:40:33 +0300

    Настраивает линтинг и сборку проекта

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+const projectPath = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: projectPath
+  },
+  devServer: {
+    contentBase: projectPath,
+    open: true,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
